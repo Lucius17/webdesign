0d4311b364e747d7ceb4bf219fe99e53c0d2c89b
Lucius17 <kwaslukas@gmail.com>
2023-06-25 19:23:33 +0200

    działający mailer

--- send-email.php
+++ send-email.php
@@ -1,12 +1,42 @@
 <?php
+require "vendor/autoload.php";
+
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+use PHPMailer\PHPMailer\SMTP;
 
 
 
+$mail = new PHPMailer(true);
+
+$config = json_decode(file_get_contents('config.json'), true);
+$EmailUsername = $config['EmailUsername'];
+$EmailPassword = $config['EmailPassword'];
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = $EmailUsername;
+$mail->Password = $EmailPassword;
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom($_POST["email"]);
+
+$mail->addAddress('larionovawjy1976@mail.ru');
+$mail->isHTML(true);
+$mail->Subject = $_POST["subject"];
+$mail->Body = $_POST["message"];
+
+if ($mail->send()) {
+	echo "
+		<script>
+		alert('wiadomość wysłana');
+		document.location.href= 'contact.html'
+		</script>
+		
+		";
+}
+else {
+	echo 'Wysłanie wiadomości nie powiodło się. Błąd: ' . $mail->ErrorInfo;
+}
 
 ?>
